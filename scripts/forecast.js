5ee98d206f6300a4998bdccd93d89b0bce4f61d0
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-04-26 10:19:33 +0200

    Get Weather API Call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,17 @@
 const key = 'iT4JbARuSG0AhIozgVBAU4FssTPJOJfa';
 
+// Get weather information
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+// Get city information
 const getCity = async (city) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
@@ -10,4 +22,9 @@ const getCity = async (city) => {
     return data[0];
 };
 
-getCity('tirana').then(data => console.log(data)).catch(err => console.log(err));
+getCity('tirana')
+    .then(data => {
+        return getWeather(data.Key);
+    }).then(data => {
+        console.log(data);
+    }).catch(err => console.log(err));
